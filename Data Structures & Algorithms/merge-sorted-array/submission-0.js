c299f1372181fb7a83357478329254bc462a088e
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2026-04-26 19:40:11 +0300

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums1
+     * @param {number} m
+     * @param {number[]} nums2
+     * @param {number} n
+     * @return {void} Do not return anything, modify nums1 in-place instead.
+     */
+    merge(nums1, m, nums2, n) {
+        const nums1Copy = nums1.slice(0, m);
+        let i = 0;
+        let j = 0;
+        let idx = 0;
+
+        while (idx < m + n) {
+            if (j >= n || (i < m && nums1Copy[i] <= nums2[j])) {
+                nums1[idx++] = nums1Copy[i++];
+            } else {
+                nums1[idx++] = nums2[j++];
+            }
+        }
+    }
+}
