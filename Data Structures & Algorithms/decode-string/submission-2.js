379a6a4c45781f42c451017e9ceb889b5b88e255
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2026-05-11 17:46:03 +0300

    Add: decode-string - submission-2

---
+++ Data Structures & Algorithms/decode-string/submission-2.js
@@ -0,0 +1,32 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {string}
+     */
+    decodeString(s) {
+        let stringStack = [];
+        let countStack = [];
+        let cur = '';
+        let k = 0;
+
+        for (let c of s) {
+            if (!isNaN(c)) {
+                k = k * 10 + parseInt(c, 10);
+            } else if (c === '[') {
+                stringStack.push(cur);
+                countStack.push(k);
+                cur = '';
+                k = 0;
+            } else if (c === ']') {
+                const temp = cur;
+                cur = stringStack.pop();
+                const count = countStack.pop();
+                cur += temp.repeat(count);
+            } else {
+                cur += c;
+            }
+        }
+
+        return cur;
+    }
+}
