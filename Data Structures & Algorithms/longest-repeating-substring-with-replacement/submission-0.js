8e48a03886012d547325d198cfceba1bc4799db7
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2026-05-13 23:13:49 +0300

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {number} k
+     * @return {number}
+     */
+    characterReplacement(s, k) {
+        const charSet = new Set(s);
+        let res = 0;
+
+        for (let c of s) {
+            let l = 0;
+            let count = 0;
+
+            for (let r = 0; r < s.length; r++) {
+                if (s[r] === c) {
+                    count++;
+                }
+
+                while (r - l + 1 - count > k) {
+                    if (s[l] === c) {
+                        count--;
+                    }
+
+                    l++;
+                }
+
+                res = Math.max(res, r - l + 1);
+            }
+        }
+
+        return res;
+    }
+}
