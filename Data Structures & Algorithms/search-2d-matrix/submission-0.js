b1c7b326abbe14853c1f4c5db230d98fcd3a4329
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2026-06-04 13:34:24 +0300

    Add: search-2d-matrix - submission-0

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-0.js
@@ -0,0 +1,48 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        const ROWS = matrix.length;
+        const COLS = matrix[0].length;
+
+        let top = 0;
+        let bot = ROWS - 1;
+
+        while (top <= bot) {
+            const row = Math.floor((top + bot) / 2);
+
+            if (target > matrix[row][COLS - 1]) {
+                top = row + 1;
+            } else if (target < matrix[row][0]) {
+                bot = row - 1;
+            } else {
+                break;
+            }
+        }
+
+        if (!(top <= bot)) {
+            return false;
+        }
+
+        const row = Math.floor((top + bot) / 2);
+        let l = 0;
+        let r = COLS - 1;
+
+        while (l <= r) {
+            const mid = Math.floor((r + l) / 2);
+
+            if (target > matrix[row][mid]) {
+                l = mid + 1;
+            } else if (target < matrix[row][mid]) {
+                r = mid - 1;
+            } else {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
