b036ea66b6fdd5de1f64ebb14252783f047caa4b
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2026-06-15 22:49:46 +0300

    Add: search-in-rotated-sorted-array-ii - submission-2

---
+++ Data Structures & Algorithms/search-in-rotated-sorted-array-ii/submission-2.js
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {boolean}
+     */
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l <= r) {
+            let m = Math.floor(l + (r - l) / 2);
+
+            if (nums[m] === target) {
+                return true;
+            }
+            
+            if (nums[l] < nums[m]) {
+                if (nums[l] <= target && target < nums[m]) {
+                    r = m - 1;
+                } else {
+                    l = m + 1;
+                }
+            } else if (nums[l] > nums[m]) {
+                if (nums[m] < target && target <= nums[r]) {
+                    l = m + 1;
+                } else {
+                    r = m - 1;
+                }
+            } else {
+                l++;
+            }
+        }
+
+        return false;
+    }
+}
