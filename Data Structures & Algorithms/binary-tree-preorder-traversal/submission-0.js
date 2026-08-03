88b66ded099949f5611b50708236b3d1dd106e64
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2026-08-04 00:37:29 +0300

    Add: binary-tree-preorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-preorder-traversal/submission-0.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    preorderTraversal(root) {
+        const res = [];
+        const stack = [];
+        let cur = root;
+
+        while (cur !== null || stack.length > 0) {
+            if (cur) {
+                res.push(cur.val);
+                stack.push(cur.right);
+                cur = cur.left;
+            } else {
+                cur = stack.pop();
+            }
+        }
+
+        return res;
+    }
+}
