fbd4e7b01e0ebfd0058168a66e8d73bdfc57611f
Vladislav Kuznetsov <super.ewew2015@yandex.ru>
2026-08-04 17:28:54 +0300

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[]}
+     */
+    postorderTraversal(root) {
+        const result = [];
+
+        const postorder = (node) => {
+            if (node === null) return;
+
+            postorder(node.left);
+            postorder(node.right);
+            result.push(node.val);
+        };
+
+        postorder(root);
+
+        return result;
+    }
+}
